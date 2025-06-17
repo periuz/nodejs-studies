4e96514e223a11896d661cf183809dda90124ab7
thiago.perius <thiagoperiussilva@gmail.com>
2025-06-17 08:25:52 -0300

    new: config server and database for api rest

--- 02-api-rest-course/src/database.ts
+++ 02-api-rest-course/src/database.ts
@@ -1,9 +1,17 @@
 import { knex as setupKnex } from 'knex';
+import fs from 'node:fs';
+import path from 'node:path';
+
+// Garante que a pasta 'temp' existe
+const tempDir = path.resolve('temp');
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir);
+}
 
 export const knex = setupKnex({
-    client: 'sqlite3',
-    connection: {
-        filename: './temp/app.db'
-    },
-    useNullAsDefault: true
-})
+  client: 'sqlite3',
+  connection: {
+    filename: path.join(tempDir, 'app.db'),
+  },
+  useNullAsDefault: true,
+});
--- 02-api-rest-course/src/server.ts
+++ 02-api-rest-course/src/server.ts
@@ -3,16 +3,24 @@ import { knex } from './database';
 
 const app = fastify();
 
-app.get('/', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+app.get('/hello', async () => {
+  // Cria tabela se não existir
+  const exists = await knex.schema.hasTable('users');
+  if (!exists) {
+    await knex.schema.createTable('users', (table) => {
+      table.increments('id');
+      table.string('name');
+    });
 
-  return tables;
+    // Inserindo um usuário de exemplo
+    await knex('users').insert({ name: 'Thiago' });
+  }
+
+  // Retorna todos os usuários
+  const users = await knex('users').select('*');
+  return users;
 });
 
-app
-  .listen({
-  port: 3333,
-
-}).then (() => {
-    console.log('Server is running on http://localhost:3333')
-})
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running on http://localhost:3333');
+});
